ecf69f4ef5d36b88313d3fbe5daac3cadf82cf31
larcken <amp@naver.com>
2024-11-29 10:30:46 +0900

    영화검색 비동기로 실습

--- 8.비동기/src/Example3.svelte
+++ 8.비동기/src/Example3.svelte
@@ -1,5 +1,46 @@
 <script>
-  
-  
+    import axios from 'axios'
+  import { select_option } from 'svelte/internal';
+
+    // omdb 사이트에 발급받은 api key
+    let apikey = "77d38320";
+    let title = "";
+    let movies = null;
+    let error = null;
+    let loading = false;
+
+    async function searchMovies() {
+        // loading 중에는 다시 실행되지 않도록 함
+        if (loading) {
+            return;
+        }
+        movies = null;
+        error = null;
+        loading = true;
+        try {
+            const res = await axios.get(`http://www.omdbapi.com/?apikey=${apikey}&s=${title}`)
+            console.log(res)
+            movies = res.data.Search;
+        } catch(e) {
+            error = e;
+        } finally {
+            loading = false;
+        }
+    }
 
 </script>
+
+<input bind:value={title} />
+<button on:click={searchMovies}>검색!</button>
+
+{#if loading}
+    <p style='color: royalblue'>Loading...</p>
+{:else if movies}
+    <ul>
+        {#each movies as movie}
+            <li>{movie.Title}</li>
+        {/each}
+    </ul>
+{:else if error}
+    <p style='color:red;'>{error.message}</p>
+{/if}
